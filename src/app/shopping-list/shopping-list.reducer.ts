fbf0329946e91173f1e06b630d01d48d6b0470d3
Sergey Pashnin <s.pashnin@encry.ru>
2019-11-06 14:57:51 +0300

    first commit

---
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,26 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { Action } from '@ngrx/store';
+
+const
+  initialState = {
+    ingredients: [
+      new Ingredient('Apples', 5),
+      new Ingredient('Tomatoes', 10),
+    ]
+  };
+
+export function shoppingListReducer(state = initialState, action: Action) {
+
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      //// todo нкльзя редактировать существующее или предыдущее состояние. Возвращаем новый объект
+      return {
+        ...state, // копируем все старые состояния (добавляем к новому объекту)
+        ingredients: [
+          ...state.ingredients,
+          action,
+        ]
+      };
+
+  }
+}
