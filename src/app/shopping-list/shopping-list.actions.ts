31146f96e667953427466789ea5e78116f111963
Sergey Pashnin <s.pashnin@encry.ru>
2019-11-08 09:09:57 +0300

    add   UpdateIngredient + DeleteIngredient;

--- src/app/shopping-list/shopping-list.actions.ts
+++ src/app/shopping-list/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
@@ -10,7 +12,6 @@ export class AddIngredient implements Action {
   constructor(
     public payload: Ingredient,
   ) {
-
   }
 }
 
@@ -20,8 +21,33 @@ export class AddIngredients implements Action {
   constructor(
     public payload: Ingredient[],
   ) {
-
   }
 }
 
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export class UpdateIngredient implements Action {
+  readonly type = UPDATE_INGREDIENT;
+
+  constructor(
+    public payload: {
+      index: number,
+      ingredient: Ingredient,
+    }
+  ) {
+  }
+}
+
+export class DeleteIngredient implements Action {
+  readonly type = DELETE_INGREDIENT;
+
+  constructor(
+    public payload: { index: number, },
+  ) {
+  }
+}
+
+
+export type ShoppingListActions =
+  AddIngredient |
+  AddIngredients |
+  UpdateIngredient |
+  DeleteIngredient;
